73d6303be9317e015815bd1ac8fbc2a852e79069
Ankan Nayek <ankannayek002@gmail.com>
2026-03-29 12:43:33 +0530

    Change server port to 8000 for app.run

--- app.py
+++ app.py
@@ -351,5 +351,4 @@ def predict_skin_disease():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8000)
